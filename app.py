b058024c0e7f917364f8b3341828d0d02bac68bc
alpha-ze <149688827+alpha-ze@users.noreply.github.com>
2025-07-11 00:42:15 +0530

    app.py

--- app.py
+++ app.py
@@ -127,4 +127,5 @@ def run_schedule():
 
 # === RUN FLASK APP (FOR SAME NETWORK USE) ===
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
+
